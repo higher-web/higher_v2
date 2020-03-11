2b647bc5f2294ba3d62536361b0a7da2c8880166
Aldana cortez <aldana.cortez17@gmail.com>
2020-03-11 11:00:20 -0300

    crea modulo arrow2

--- js/arrowup.js
+++ js/arrowup.js
@@ -5,7 +5,7 @@ $(document).ready(function () { //Hacia arriba
 function irArriba() {
   $('.ir-arriba').click(function () { $('body,html').animate({ scrollTop: '0px' }, 1000); });
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 0) { $('.ir-arriba').slideDown(600); } else { $('.ir-arriba').slideUp(600); }
+    if ($(this).scrollTop() > 700) { $('.ir-arriba').slideDown(800); } else { $('.ir-arriba').slideUp(800); }
   });
   $('.ir-abajo').click(function () { $('body,html').animate({ scrollTop: '1000px' }, 1000); });
 }
